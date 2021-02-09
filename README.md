61adc37df3e38dcbb9ec377d6ba40e9d3b775dd8
Almonzo Rhoden <almonzo.rhoden@gmail.com>
2021-02-09 05:15:00 -0500

    uploading files

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# team-generator
+
+<h1 align = "center" > Team Generator </h1>
+
+<h2>Preview of application<h2>
+
+![Application](https://user-images.githubusercontent.com/61447353/107347921-580ec880-6a94-11eb-8814-98c5b5143bc1.PNG)
+
+<h2> Application descripition </h2>
+
+An individual project, an application that will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns.
+
+<h2>Deployed application</h2>
+https://almonzorhoden.github.io/team-generator/ 
+
+<h2>Installation</h2>
+Once the application is downloaded, run the following commands in the terminal at the root of your project folder: 
+
+     `node app.js`
+
+
+Afterwards, go to the output folder and double click on "output.html" file and it should open up what you just made in browser:
+
+    `Double click on output.html or right click and select preview in default browser
+    
+Video Demonstration
+<iframe src="https://drive.google.com/file/d/1dHG1P8utWgIAtAe_Wqxsw186m94fNUM8/preview" width="640" height="480"></iframe>
+
+## Questions?
+Email me at almonzo.rhoden@gmail.com, so I can address any questions or concerns you may have regarding my application thank you.
---
+++ app.js
@@ -0,0 +1,210 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+const employeeArray = [];
+
+//All classes need name, id, and email
+const promptUser = async () => {
+    try {
+        //Make an object for each team member depending on what class it is
+        const answers = await inquirer.prompt([
+            {
+                type: 'list',
+                message: 'What is the role of this employee?',
+                name: 'role',
+                choices: ["Engineer", "Intern", "Manager"]
+            }
+
+        ]).then(function (answer) {
+            if (answer.role == "Manager") {
+                //Manager needs office number
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            message: 'What is the name of the employee?',
+                            name: 'name'
+                        },
+                        {
+                            type: 'input',
+                            message: "What is the employee's id number?",
+                            name: 'id',
+                            //Verify that it is a number
+                            validate: function(value) {
+                                if (isNaN(value) === false) {
+                                  return true;
+                                }
+                                return false;
+                              }
+                        },
+                        {
+                            type: 'input',
+                            message: "What is the employee's email address?",
+                            name: 'email',
+                            validate: function(value){
+                                if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)){
+                                    return true;
+                                }
+                                return false;
+                            }
+                        },
+                        {
+                            type: 'input',
+                            message: "What is the manager's office number?",
+                            name: 'officenumber',
+                            validate: function(value) {
+                                if (isNaN(value) === false) {
+                                  return true;
+                                }
+                                return false;
+                              }
+                        }
+                    ]).then(answer => {
+
+                        let newEmployee = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+                        employeeArray.push(newEmployee);
+                        startOver(employeeArray);
+
+                    })
+            } else if (answer.role == "Engineer") {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            message: 'What is the name of the employee?',
+                            name: 'name'
+                        },
+                        {
+                            type: 'input',
+                            message: "What is the employee's id number?",
+                            name: 'id',
+                            validate: function(value) {
+                                if (isNaN(value) === false) {
+                                  return true;
+                                }
+                                return false;
+                              }
+                        },
+                        {
+                            type: 'input',
+                            message: "What is the employee's email address?",
+                            name: 'email',
+                            validate: function(value){
+                                if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)){
+                                    return true;
+                                }
+                                return false;
+                            }
+                        },
+                        {
+                            type: 'input',
+                            message: "What is the engineer's github username?",
+                            name: 'github_username'
+                        }
+                    ]).then(function (answer) {
+                        //Engineer also needs github username
+                        let newEmployee = new Engineer(answer.name, answer.id, answer.email, answer.github_username);
+                        employeeArray.push(newEmployee);
+
+                        startOver(employeeArray);
+                    })
+            } else  {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            message: 'What is the name of the employee?',
+                            name: 'name'
+                        },
+                        {
+                            type: 'input',
+                            message: "What is the employee's id number?",
+                            name: 'id',
+                            validate: function(value) {
+                                if (isNaN(value) === false) {
+                                  return true;
+                                }
+                                return false;
+                              }
+                        },
+                        {
+                            type: 'input',
+                            message: "What is the employee's email address?",
+                            name: 'email',
+                            validate: function(value){
+                                if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)){
+                                    return true;
+                                }
+                                return false;
+                            }
+                        },
+                        {
+                            type: 'input',
+                            message: "What school does the intern attend?",
+                            name: 'school'
+                        }
+                    ]).then(answer => {
+
+                        let newEmployee = new Intern(answer.name, answer.id, answer.email, answer.school);
+
+                        employeeArray.push(newEmployee);
+
+                        startOver(employeeArray);
+                    });
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+promptUser();
+
+
+const startOver = array => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Do you want to add another employee?',
+                name: "yesno",
+                choices: ["yes", "no"],
+            }
+        ]).then(answer => {
+            if (answer.yesno == "yes") {
+                promptUser();
+            } else {
+                render(array);
+            }
+        })
+}
+
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
